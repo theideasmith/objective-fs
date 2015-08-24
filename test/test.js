7c7eb9041a52f1bcdda164e99454065e507f5cc8
aclinnovator <aclscientist@gmail.com>
2015-08-23 22:28:34 -0400

    tests for searchObject stack pass

--- test/test.js
+++ test/test.js
@@ -1,31 +1,31 @@
 var mocha = require('mocha')
 var should = require('should')
 
-Array.prototype.equals = function(bs){
+Array.prototype.equals = function(bs) {
   var as = this
   var a, b
-  while((a = as.shift()) && (b = bs.shift())) {
-    if (a !==b )return false
+  while ((a = as.shift()) && (b = bs.shift())) {
+    if (a !== b) return false
   }
   return true
 }
 
-function testFailure(func){
+function testFailure(func) {
   if (!func) throw new Error("No function supplied")
-  try{
+  try {
     func()
-  } catch(err){
+  } catch (err) {
     return true
   }
   return false
 }
 
-describe('objective-fs', function(){
+describe('objective-fs', function() {
   var ofs = require('../src/objective-fs');
 
   var data = {
     x: {
-      y:{
+      y: {
         z: "abc"
       },
       a: {
@@ -36,54 +36,88 @@ describe('objective-fs', function(){
     },
 
     DNA: ['ACT', 'GAT', 'CTT'],
-    "20":15
+    "20": 15
   }
 
   var dataKeys = Object.keys(data)
 
   var datafs = ofs(data)
 
-  describe('#cat', function(){
+  describe("#_searchObject", function() {
 
-    it("should return the correct object given a regular path", function(){
+    function testStack(path, shouldStack) {
+      var stack = datafs._searchObject(path).dirs
+
+      stack.forEach(function(obj, ind) {
+        obj.should.deepEqual(shouldStack[ind])
+      })
+    }
+
+    it("should create the correct stack for a regular path", function() {
+      testStack("/x/a/b", [
+        data,
+        data.x,
+        data.x.a,
+        data.x.a.b
+      ])
+
+    })
+
+    it("should create the correct stack for a superdirectory path", function() {
+
+      testStack("/x/y/../../DNA/0", [
+        data,
+        data.DNA,
+        data.DNA[0]
+      ])
+
+    })
+
+  })
+
+  describe('#cat', function() {
+
+    it("should return the correct object given a regular path", function() {
       datafs.cat('x/y/z').should.equal(data.x.y.z)
     })
 
-    it("should return the correct object for a root path", function(){
+    it("should return the correct object for a root path", function() {
       datafs.cat('/x/y/z').should.equal(data.x.y.z)
     })
 
-    it("should return the correct object given an array index", function(){
+    it("should return the correct object given an array index", function() {
       datafs.cat('/DNA/0/').should.equal(data.DNA[0])
     })
 
-    it("should return the base object given HOME ('~') path", function(){
+    it("should return the base object given HOME ('~') path", function() {
       Object.keys(datafs.cat('~')).equals(dataKeys).should.equal(true)
     })
 
-    it("should return the correct object with superdirectories", function(){
+    it("should return the correct object with superdirectories", function() {
       datafs.cat('/x/y/../../DNA/0').should.equal('ACT')
     })
 
-    describe("failures", function(){
-      it("should fail upon hitting an invalid superdirectory", function(){
-        testFailure(function(){datafs.cat('/x/y/../../../')}).should.equal(true)
+    describe("failures", function() {
+      it("should fail upon hitting an invalid superdirectory", function() {
+        testFailure(function() {
+          datafs.cat('/x/y/../../../')
+        }).should.equal(true)
       })
 
-      it("should fail when given an invalid path", function(){
-        testFailure(function(){
+      it("should fail when given an invalid path", function() {
+        testFailure(function() {
           datafs.cat('./20/15')
         }).should.equal(true)
 
-        testFailure(function(){
+        testFailure(function() {
           datafs.cat('./x/3')
         }).should.equal(true)
       })
     })
   })
 
-  describe('#cd', function(){
-    it('should navigate to a passed directory', function(){
+  describe('#cd', function() {
+    it('should navigate to a passed directory', function() {
       var path = './x/a/'
       var shouldEqual = data.x.a
       datafs.cd(path)
